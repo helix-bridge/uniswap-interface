bc6c747bd36b3d587f9a5734bbf4c555a5c7d7ec
JayJay1024 <lygm1062@gmail.com>
2024-07-03 11:15:05 +0800

    chore: update interface scrop

--- scripts/web-checks-should-run.sh
+++ scripts/web-checks-should-run.sh
@@ -3,7 +3,7 @@
 # This script controls whether the web quality checks run in Github
 
 # see: https://github.com/vercel/turbo/blob/main/packages/turbo-ignore/README.md
-npx turbo-ignore @uniswap/interface --fallback=HEAD^
+npx turbo-ignore @helix-bridge/interface --fallback=HEAD^
 
 # we get exit 1 if, in the diff of the current branch vs main,
 # there was a change in web (or a sub-dependency of web)
