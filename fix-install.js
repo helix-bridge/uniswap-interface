d34ff6750702f0f3dbf64fde23897b4d124f21da
JayJay1024 <lygm1062@gmail.com>
2024-07-04 18:57:24 +0800

    chore: update fix-install.js

--- fix-install.js
+++ fix-install.js
@@ -1,31 +1,11 @@
 const fs = require("fs");
 const path = require("path");
 
-const flag = process.argv.at(2);
-const filePath = path.join(__dirname, "package.json");
-const backFilePath = path.join(__dirname, "package.json.bak");
+const packageFilePath = path.join(__dirname, "package.json");
+const resPackageFilePath = path.join(__dirname, "package.json.res");
 
-if (flag === "0") {
-  fs.copyFileSync(filePath, backFilePath);
+const lockFilePath = path.join(__dirname, "yarn.lock");
+const resLockFilePath = path.join(__dirname, "yarn.lock.res");
 
-  const data = fs.readFileSync(filePath, "utf-8");
-  const packageJson = JSON.parse(data);
-
-  delete packageJson.resolutions["@uniswap/v2-sdk"];
-  delete packageJson.resolutions["@uniswap/v3-sdk"];
-  delete packageJson.resolutions["@uniswap/router-sdk"];
-  delete packageJson.resolutions["@uniswap/uniswapx-sdk"];
-  delete packageJson.resolutions["@uniswap/sdk-core"];
-  delete packageJson.resolutions["@uniswap/universal-router-sdk"];
-  delete packageJson.resolutions["@uniswap/smart-order-router"];
-
-  const updatedPackageJson = JSON.stringify(packageJson, null, 2);
-  fs.writeFileSync(filePath, updatedPackageJson, "utf-8");
-
-  // console.info("package.json updated successfully.");
-} else if (flag === "1") {
-  fs.copyFileSync(backFilePath, filePath);
-  fs.unlinkSync(backFilePath);
-
-  // console.info("package.json restored successfully.");
-}
+fs.copyFileSync(resPackageFilePath, packageFilePath);
+fs.copyFileSync(resLockFilePath, lockFilePath);
