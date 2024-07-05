a4b9168af344c495534a26dfc28544973c7c27c6
JayJay1024 <lygm1062@gmail.com>
2024-07-05 13:32:44 +0800

    feat: brc and wbtc token logo

--- apps/web/src/lib/hooks/useCurrencyLogoURIs.ts
+++ apps/web/src/lib/hooks/useCurrencyLogoURIs.ts
@@ -7,8 +7,11 @@ import AvaxLogo from '../../assets/svg/avax_logo.svg'
 import BnbLogo from '../../assets/svg/bnb-logo.svg'
 import CeloLogo from '../../assets/svg/celo_logo.svg'
 import MaticLogo from '../../assets/svg/matic-token-icon.svg'
+import BRCLogo from '../../assets/svg/brc.svg'
 import BTCLogo from '../../assets/svg/btc.svg'
-import { PORTAL_ETH_CELO, isCelo, nativeOnChain } from '../../constants/tokens'
+import WBTCLogo from '../../assets/svg/wbtc.svg'
+import { PORTAL_ETH_CELO, isBitlayer, isCelo, nativeOnChain } from '../../constants/tokens'
+import { BRC_BITLAYER_TESTNET, USDC_BITLAYER_TESTNET, USDT_BITLAYER_TESTNET, WBTC_BITLAYER_TESTNET } from '@uniswap/smart-order-router'
 
 export function getNativeLogoURI(chainId: ChainId = ChainId.MAINNET): string {
   switch (chainId) {
@@ -37,6 +40,19 @@ export function getTokenLogoURI(address: string, chainId: ChainId = ChainId.MAIN
   }
   if (isCelo(chainId) && isSameAddress(address, PORTAL_ETH_CELO.address)) {
     return EthereumLogo
+  }
+
+  if (isBitlayer(chainId) && isSameAddress(address, USDC_BITLAYER_TESTNET.address)) {
+    return 'https://raw.githubusercontent.com/Uniswap/assets/master/blockchains/ethereum/assets/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48/logo.png'
+  }
+  if (isBitlayer(chainId) && isSameAddress(address, USDT_BITLAYER_TESTNET.address)) {
+    return 'https://raw.githubusercontent.com/Uniswap/assets/master/blockchains/ethereum/assets/0xdAC17F958D2ee523a2206206994597C13D831ec7/logo.png'
+  }
+  if (isBitlayer(chainId) && isSameAddress(address, WBTC_BITLAYER_TESTNET.address)) {
+    return WBTCLogo
+  }
+  if (isBitlayer(chainId) && isSameAddress(address, BRC_BITLAYER_TESTNET.address)) {
+    return BRCLogo
   }
 
   if (networkName) {
