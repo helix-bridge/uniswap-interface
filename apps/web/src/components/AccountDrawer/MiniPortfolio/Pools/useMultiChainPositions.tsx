8aebf2953dcbcc4b7a461c27259a4aea552e7f73
JayJay1024 <lygm1062@gmail.com>
2024-07-07 22:19:17 +0800

    refactor: update chains on useMultiChainPositions

--- apps/web/src/components/AccountDrawer/MiniPortfolio/Pools/useMultiChainPositions.tsx
+++ apps/web/src/components/AccountDrawer/MiniPortfolio/Pools/useMultiChainPositions.tsx
@@ -42,7 +42,8 @@ type FeeAmounts = [BigNumber, BigNumber]
 const MAX_UINT128 = BigNumber.from(2).pow(128).sub(1)
 
 const DEFAULT_CHAINS = [...L1_CHAIN_IDS, ...L2_CHAIN_IDS].filter((chain: number) => {
-  return !TESTNET_CHAIN_IDS.includes(chain) || isBitlayer(chain)
+  // return !TESTNET_CHAIN_IDS.includes(chain)
+  return isBitlayer(chain)
 })
 
 type UseMultiChainPositionsData = { positions?: PositionInfo[]; loading: boolean }
